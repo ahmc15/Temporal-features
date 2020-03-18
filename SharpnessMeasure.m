db68b5d028959c21499a9d79930302c6b3f5f935
ahmc15 <andrehmdacosta15@gmail.com>
2020-03-18 17:48:31 -0300

    Create gamma filter

--- SharpnessMeasure.m
+++ SharpnessMeasure.m
@@ -1,6 +1,18 @@
-function [ output_args ] = Untitled2( input_args )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [ Sharpness_score ] = Sharpness(patch)
+%Calculates sharpness through the variance field of the image
+%  Detailed explanation goes here
+%
+%
+if (nargin <1)
+error('First argument (patch) is required');    
+end
+
+
+
+gamma_filter = fspecial('gaussian',7,7/6);
+gamma_filter = gamma_filter/sum(sum(gamma_filter));
+
+
 
 
 end
