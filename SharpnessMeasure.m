7265f02e78b72fd24fb305c0fee593edf0939aff
André da Costa <andrehmdacosta15@gmail.com>
2020-03-19 09:01:05 -0300

    calculates variance field

--- SharpnessMeasure.m
+++ SharpnessMeasure.m
@@ -6,12 +6,18 @@ function [ Sharpness_score ] = Sharpness(patch)
 if (nargin <1)
 error('First argument (patch) is required');    
 end
+[row, col] = size(patch);
 
-
+mean=[];
+variance=[];
+patch_sq=patch.*patch;
 
 gamma_filter = fspecial('gaussian',7,7/6);
 gamma_filter = gamma_filter/sum(sum(gamma_filter));
 
+mean = imfilter(patch, gamma_filter, 'replicate');
+mean_sq = mean.*mean;
+sigma = sqrt(abs(imfilter(patch_sq,gamma_filter, 'replicate')-mean_sq));
 
 
 
