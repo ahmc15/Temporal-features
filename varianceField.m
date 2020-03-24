e94dea0b4ab661d5595bc819b9c1ebf0bbd59f26
André da Costa <andrehmdacosta15@gmail.com>
2020-03-24 16:48:46 -0300

    tested

--- varianceField.m
+++ varianceField.m
@@ -1,45 +1,50 @@
-function [ framedifference, Media, sigma ] = varianceField( frame, nextframe, direction )
+function [ framedifferencefinal, Mediafinal, sigmafinal ] = varianceField( frame, nextframe, direction )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 if (nargin <3)
     error('Function requires 3 arguments');
 end
 [nrows,ncol] = size(frame);
-framedifference = zeros(nrows,ncols);
+framedif = zeros(nrows,ncol);
 
 switch direction
-    case horizontal
+    case 'horizontal'
         for j=2:ncol-1
             for i=2:nrows
-                framedifference = frame(i,j)-nextframe(i,j-1);
+                framedif(i,j) = frame(i,j)-nextframe(i,j-1);                
             end
         end
-    case vertical
+    case 'vertical'
          for j=2:ncol-1
             for i=2:nrows
-                framedifference = frame(i,j)-frameT1(i-1,j);
+                framedif(i,j) = frame(i,j)-nextframe(i-1,j);
             end
          end
-    case diagonal1
+    case 'diagonal1'
         for j=2:ncol-1
             for i=2:nrows
-                framedifference = frame(i,j)-nextframe(i-1,j-1);
+                framedif(i,j) = frame(i,j)-nextframe(i-1,j-1);
             end
         end
-    case diagonal2
+    case 'diagonal2'
         for j=2:ncol-1
             for i=2:nrows
-                framedifference = frame(i,j)-nextframe(i-1,j+1);
+                framedif(i,j) = frame(i,j)-nextframe(i-1,j+1);
             end
         end
-
-framedifference = framedifference(2:end,2:end-1);
-[nrows,ncol] = size(framedifference);
+end
+framedif = framedif(2:end,2:end-1);
+[nrows,ncol] = size(framedif);
 
 gamma_filter_mask = fspecial('gaussian',7,7/6);
-gamma_filter_mask = gamma_filter_mask/sum(sum(gamma_filter_mask));
-Media = imfilter(framedifference, gamma_filter_mask);
-sigma = sum(sum((framedifference-Media).^2),1),2);
-sigma = repmat(sigma, [nrows,ncols]);
+gamma_filter_mask = gamma_filter_mask./sum(sum(gamma_filter_mask));
+Media = imfilter(framedif, gamma_filter_mask);
+sigma = sum(sum(((framedif-Media).^2),1),2);
+sigma = repmat(sigma, [nrows,ncol]);
+
+framedifferencefinal=framedif;
+Mediafinal = Media;
+sigmafinal = sigma;
+
 end
 
