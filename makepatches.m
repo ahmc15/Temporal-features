5c553bea0de6dcb4d8fda44635317d1b87a61f87
André da Costa <andrehmdacosta15@gmail.com>
2020-03-24 18:01:01 -0300

    cleaning and add comments

--- makepatches.m
+++ makepatches.m
@@ -1,6 +1,7 @@
 function [ ArrayPatches ] = makepatches( varianceField )
 %Divides de variance field into a series of patches 96x96
-%   The function takes a variance field and return patches of size 96x96
+%   The function takes a variance field and return patches of size 96x96.
+%   Each patch is stored in the third dimension of the output.
 [rows cols] = size(varianceField);
 borderRows = rem(rows,96)/2;
 borderCols = rem(cols,96)/2;
@@ -9,15 +10,6 @@ borderless = varianceField(borderRows+1:end-borderRows, borderCols+1:end-borderC
 [brows, bcols] = size(borderless);
 listNum =(size(borderless,1)*size(borderless,2)/(96*96));
 patchlist = zeros(96,96,listNum);
-
-% for j=(1:96:end)
-%     for i =(1:96:end)
-%         patch = (i:i+96,j:j+96);
-%         patchlist(:,:,n) = patch;
-%         patchlist = cat(3,patchlist,patch) 
-%     end
-% end
-
 
 for n=1:listNum
     for j=1:96:brows-96
@@ -29,11 +21,5 @@ for n=1:listNum
 end
 
 ArrayPatches = patchlist;
-%dividir dimensões por tamanho do patch
-%achar primeiro pixel do primeiro patch
-%fazer lista de patches
-
-
-
 end
 
