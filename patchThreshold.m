80db6dff66a78f0fee61f10e6d5ebb5c81c41fcb
André da Costa <andrehmdacosta15@gmail.com>
2020-03-24 18:22:59 -0300

    added description

--- patchThreshold.m
+++ patchThreshold.m
@@ -1,11 +1,13 @@
 function [ patchesTrue ] = patchThreshold( patchs )
 %UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%   Checks fi the patches have a variance value above de threshold. The
+%   choosen threshold is 25% of the maximum variance value. 
+%
+%   The output is the concatenation of all the patches that meet the threshold criteria.
 
 meanSigmaPatchs = mean(mean(patchs,2),1);
 threshold = 0.75*max(max(max(meanSigmaPatchs,[],1),[],2),[],3);
 patchesTrue = zeros(96,96);
-
 
 for i=1:size(meanSigmaPatchs,3)
     if meanSigmaPatchs(:,:,i)>=threshold
