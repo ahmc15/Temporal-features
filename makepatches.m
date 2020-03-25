c14924b02c43eb4224cad967fa78880c5b98ea8c
André da Costa <andrehmdacosta15@gmail.com>
2020-03-24 22:15:23 -0300

    correction to for loop

--- makepatches.m
+++ makepatches.m
@@ -2,6 +2,7 @@ function [ ArrayPatches ] = makepatches( varianceField )
 %Divides de variance field into a series of patches 96x96
 %   The function takes a variance field and return patches of size 96x96.
 %   Each patch is stored in the third dimension of the output.
+
 [rows cols] = size(varianceField);
 borderRows = rem(rows,96)/2;
 borderCols = rem(cols,96)/2;
@@ -12,8 +13,8 @@ listNum =(size(borderless,1)*size(borderless,2)/(96*96));
 patchlist = zeros(96,96,listNum);
 
 for n=1:listNum
-    for j=1:96:brows-96
-        for i =1:96:bcols-96
+    for j=1:96:bcols-96
+        for i =1:96:brows-96
             patch = borderless(i:i+95,j:j+95);
             patchlist(:,:,n) = patch; 
         end
