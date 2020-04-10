4ba1fe6b61f78a775e7c4feffac13c8d3229c416
André da Costa <andrehmdacosta15@gmail.com>
2020-04-10 18:26:48 -0300

    created function

--- temporalFeatures.m
+++ temporalFeatures.m
@@ -1,16 +1,31 @@
-function [ output_args ] = temporalFeatures( input_args )
+function [ temporal_features ] = temporalFeatures( videoname, direction )
 %Calculate de temporal features from a entire video file
 %   Detailed explanation goes here
+%   INPUT
+%         videoname: string of the video file with extension
+%   OUTPUT
+%         temporalFeatures: matrix of size (n,2). Each column is one of the
+%           two parameters of the generalized gaussian distribution of all
+%           the patches of all frames
+%   Code:
+%       here describe the code
 
-% video = VideoReader('Megamind.avi');
-% NumFrames = floor(video.FrameRate*video.Duration);
-% tempo = 30;
-% frame = rgb2gray(read(video,tempo)); %frame atual
-% nextframe= rgb2gray(read(video,tempo+1));
-% [ difference, meanLocal, varianceField ] = varianceField( frame, nextframe, 'horizontal' );
-% [pd, pm] = temporalPatches( difference, meanLocal );
-% [ diff, meanPat ] = temporalPatchThreshold( pd, pm);
-% [ MSCN ] = temporalMSCN( diff, meanPat, varianceField );
-% [ ggdParameters ] = ggdFitting( MSCN );
+%%
+
+video = VideoReader(videoname);
+NumFrames = floor(video.FrameRate*video.Duration);
+temporal_features = [];
+
+for tempo= 1:NumFrames-1
+    
+    frame = rgb2gray(read(video,tempo)); %frame atual
+    nextframe= rgb2gray(read(video,tempo+1));
+    [ difference, meanLocal, variancefield ] = varianceField( frame, nextframe, direction );
+    [pd, pm] = temporalPatches( difference, meanLocal );
+    [ diff, meanPat ] = temporalPatchThreshold( pd, pm);
+    [ MSCN ] = temporalMSCN( diff, meanPat, variancefield );
+    [ ggdParameters ] = ggdFitting( MSCN );
+    temporal_features = [temporal_features; ggdParameters];
+    
 end
 
