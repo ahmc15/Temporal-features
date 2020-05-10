ab17e3cae149d497bffaae1920a5824c4e839fd9
ahmc15 <andrehmdacosta15@gmail.com>
2020-05-09 23:42:23 -0300

    Implemented Try func

--- runexp1.m
+++ runexp1.m
@@ -1,25 +1,56 @@
 clear all
-fds = fileDatastore('C:\Users\Adm\Desktop\Temporal-Features\Experiment1\v01packetloss\*.avi', 'ReadFcn', @importdata);
+fds = fileDatastore('C:\Users\Adm\Desktop\Temporal-Features\Varium\VariumDegradados\*.avi', 'ReadFcn', @importdata);
 FileNamesavi = fds.Files;
 fullFileNames = [FileNamesavi];
 numFiles = length(fullFileNames);
 
-pristinemodel1 = load('Exp1Horizontalv1atev45.mat');
-pristinemodel1 = pristinemodel1.Exp1HorizontalModel;
-pristinemodel = [pristinemodel1];
-direction='horizontal';
-% pristinemodel1 = temporalFeatures('C:\Users\Adm\Desktop\Temporal-Features\Experiment1\v01_ANC1.avi',direction);
-% pristinemodel2 = temporalFeatures('C:\Users\Adm\Desktop\Temporal-Features\Experiment1\v01.avi',direction);
-% pristinemodel = [pristinemodel1; pristinemodel2];
-Distances=[];
-%Loop over all files reading them in and plotting them.
+horizontalmodel=[];
+verticalmodel=[];
+diagonal1model=[];
+diagonal2model=[];
 
-for k = 1 : numFiles
-    fprintf('Now reading file %s. Files left %d\n', fullFileNames{k}(66:end), (numFiles-k));
-    video = fullFileNames{k};
-    msgmodel = temporalFeatures(video,direction);
-    distance = MahalanobisDistance(msgmodel,pristinemodel);
-    Distances=[Distances; distance]; 
+load('PristineVarium.mat');
+for i=1:size(pristinemodel,1)
+    horizontalmodel=[horizontalmodel;pristinemodel{i,2}];
+    verticalmodel=[verticalmodel;pristinemodel{i,3}];
+    diagonal1model=[diagonal1model;pristinemodel{i,4}];
+    diagonal2model=[diagonal2model;pristinemodel{i,5}];
 end
-sound(2*sin(1:10000));
+listaDirecao={'','horizontal','vertical', 'diagonal1','diagonal2'};
+listaModelos={'',horizontalmodel,verticalmodel,diagonal1model,diagonal2model};
+DistancesVarium=cell(numFiles,4);
+DistancesVarium = [fullFileNames,DistancesVarium];
+
+for j=2:5
+    for k = 1 : numFiles
+        if isempty(DistancesVarium{k,j})
+            try
+                fprintf('Now reading file %s; posistion=(%d,%d); %s\n ', fullFileNames{k}(64:1:end),k,j,listaDirecao{1,j});
+                video = fullFileNames{k};
+                msgmodel = temporalFeatures(video,listaDirecao{1,j});
+                DistancesVarium{k,j} = MahalanobisDistance(msgmodel,listaModelos{1,j});
+            catch
+                DistancesVarium{k,j} = 'error'
+            end
+        end
+    end
+    save('DistancesVarium','DistancesVarium')
+end
+
+for j=2:5
+    for k = 1 : numFiles
+        if DistancesVarium{k,j}=='error'
+            try
+                fprintf('Now reading file %s; posistion=(%d,%d); %s\n ', fullFileNames{k}(64:1:end),k,j,listaDirecao{1,j});
+                video = fullFileNames{k};
+                msgmodel = temporalFeatures(video,listaDirecao{1,j});
+                DistancesVarium{k,j} = MahalanobisDistance(msgmodel,listaModelos{1,j});
+            catch
+                DistancesVarium{k,j} = 'error'
+            end
+        end
+    end
+    save('DistancesVarium','DistancesVarium')
+end
+% sound(2*sin(1:10000));
 % save(strcat('distancia',direction),'Distances');
