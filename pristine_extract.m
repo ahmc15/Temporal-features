1ec2a80d7d1a9ece3172e503f441ac8173074f8c
ahmc15 <andrehmdacosta15@gmail.com>
2020-05-14 18:14:24 -0300

    deleted old lines

--- pristine_extract.m
+++ pristine_extract.m
@@ -1,42 +1,6 @@
 % Get a list of all txt files in the current folder, or subfolders of it.
-% clear all
 
-% direction = 'horizontal';
-% fds = fileDatastore('C:\Users\Adm\Desktop\Temporal-Features\PristineVideos\*.mp4', 'ReadFcn', @importdata);
-% FileNamesMp4 = fds.Files;
-% fds = fileDatastore('C:\Users\Adm\Desktop\Temporal-Features\PristineVideos\*.mov', 'ReadFcn', @importdata);
-% FileNamesMov = fds.Files;
-% fullFileNames = [FileNamesMp4; FileNamesMov];
-% fds = fileDatastore('C:\Users\Adm\Desktop\Temporal-Features\Experiment1\originais\*.avi', 'ReadFcn', @importdata);
-% fullFileNames = fds.Files;
-% numFiles = length(fullFileNames);
-%Loop over all files reading them in and plotting them.
-% Exp1HorizontalModel = [];
-% for k = 1 : numFiles
-%     fprintf('Now reading file %s. Files left %d\n', fullFileNames{k}(62:end), (numFiles-k));
-%     pristine = temporalFeatures(fullFileNames{k},direction);
-%     Exp1HorizontalModel  = [Exp1HorizontalModel ; pristine];
-% end
 
-% save(strcat('Exp1',direction,'files.mat'),'fullFileNames','Exp1HorizontalModel');
-% =======
-% direction = 'diagonal1';
-% 
-% fds = fileDatastore('C:\Users\Adm\Desktop\Temporal-Features\PristineVideos\*.mp4', 'ReadFcn', @importdata);
-% FileNamesMp4 = fds.Files;
-% fds = fileDatastore('C:\Users\Adm\Desktop\Temporal-Features\PristineVideos\*.mov', 'ReadFcn', @importdata);
-% FileNamesMov = fds.Files;
-% fullFileNames = [FileNamesMp4; FileNamesMov];
-% numFiles = length(fullFileNames);
-% %Loop over all files reading them in and plotting them.
-% PristineModel = [];
-% for k = 1 : numFiles
-%     fprintf('Now reading file %s. Files left %d\n', fullFileNames{k}(55:end), (numFiles-k));
-%     pristine = temporalFeatures(fullFileNames{k},direction);
-%     PristineModel  = [PristineModel ; pristine];
-% end
-% 
-% save(strcat('PristineModel',direction,'files.mat'),'PristineModel');
 clear all
 fds = fileDatastore('C:\Users\Adm\Desktop\Temporal-Features\Varium\VariumOriginais\*.avi','ReadFcn',@importdata);
 fullFileNames = fds.Files;
