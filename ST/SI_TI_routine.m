69a707fde1221ed705ba397aad480c9bf4a6c077
ahmc15 <andrehmdacosta15@gmail.com>
2020-06-11 17:00:24 -0300

    Modify small parts of the routine so it will work in my machine

--- ST/SI_TI_routine.m
+++ ST/SI_TI_routine.m
@@ -1,8 +1,7 @@
 clear all;
 clc;
 
-dirIN='H:\VideosInput\VideosInput\';
-
+dirIN='C:\Users\Adm\Desktop\Temporal-Features\Varium\VariumOriginais\';
 
 
 D = dir (strcat(dirIN,'*.avi'));
@@ -10,18 +9,24 @@ D = dir (strcat(dirIN,'*.avi'));
 
 %########### LOW ################
 vname_list=[];
+vname_list=cell(nFiles,3);
 SI_TI=[];
 
 for idx = 1:nFiles
     %input1
+    fprintf('Now reading file %s\n', D(idx).name);
     input = strcat(dirIN,D(idx).name);
     [SI, TI] = ST_perc_info_measurement(input,'max');
 
     output1=strrep(input,dirIN,'');
     vname=strrep(output1,'.avi','');
-    vname_list=[vname_list;vname];
-    SI_TI=[SI_TI;[SI, TI]];
+%     vname_list=[vname_list;vname];
+    vname_list{idx,1} = vname;
+    vname_list{idx,2} = SI;
+    vname_list{idx,3} = TI;
+%     SI_TI=[SI_TI;[SI, TI]];
 end
 
-Results=table(vname_list,SI_TI);
-writetable(Results,'SI_TI.xls');
+% Results=table(vname_list,SI_TI);
+Results=table(vname_list);
+writetable(Results,'Varium_SI_TI.xls');
