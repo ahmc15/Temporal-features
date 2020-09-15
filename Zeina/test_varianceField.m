9e40d5154c6fbe174ac8ca2dcf96fb8a13cedcf5
ahmc15 <andrehmdacosta15@gmail.com>
2020-09-15 16:12:34 -0300

    Saves variables to struct

--- Zeina/test_varianceField.m
+++ Zeina/test_varianceField.m
@@ -1,10 +1,10 @@
 % test variance Field
 % clear all
-videoname = 'C:\Users\Adm\Desktop\Temporal-Features\Databases\Experiment1\v01packetloss\v01_ANC1.avi'
+videoname = 'C:\Users\Adm\Desktop\Temporal-Features\Databases\Experiment1\v01packetloss\v01_ANC1.avi';
 video = VideoReader(videoname);
 NumFrames = countframes(video);
 video = VideoReader(videoname);
-tempo = 2;
+tempo = 100;
 direction =  'horizontal';
 
 frame = rgb2gray(read(video,tempo)); %current frame 
@@ -17,8 +17,9 @@ nextframe= rgb2gray(read(video,tempo+1));
 [ ggdParameters ] = ggdFitting( MSCN );
 temporal_features{1,1} = ggdParameters;
 
-desktopframe2 = struct();
+desktopframe100 = struct();
 myvars = who;
 for i=1:length(myvars)
-    desktopfram2.(myvars{i,1}) = eval(myvars{i,1});
+    desktopframe100.(myvars{i,1}) = eval(myvars{i,1});
 end
+save('desktopAndreframe100.mat','desktopframe100');
